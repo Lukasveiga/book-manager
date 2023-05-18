a23f207d95213030834b91f66768a552bebfe4ad
Lukas Veiga <lukas.veiga@hotmail.com>
2023-05-18 11:07:57 -0300

    unit tests: repository & create unit tests mvn profile

--- src/test/java/com/javadev/bookmanager/repository/AuthorRepositoryTest.java
+++ src/test/java/com/javadev/bookmanager/repository/AuthorRepositoryTest.java
@@ -1,10 +1,51 @@
 package com.javadev.bookmanager.repository;
 
+import com.javadev.bookmanager.entities.Author;
+import com.javadev.bookmanager.exceptions.AuthorNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class AuthorRepositoryTest {
 
+    @Autowired
+    private AuthorRepository repository;
+
+    private Author author;
+
+    @BeforeEach
+    void setup() {
+        author = new Author("Author Test", "Author Test Description");
+        repository.save(author);
+    }
+
+    @AfterEach
+    void tearDown() {
+        author = null;
+        this.repository.deleteAll();
+    }
+
+    @Test
+    void findByNameIgnoreCase_returnAuthorByName_WhenSuccessful(){
+        Author authorTest = repository.findByNameIgnoreCase(author.getName()).orElse(new Author());
+        Assertions
+                .assertThat(authorTest)
+                .isNotNull()
+                .isEqualTo(author);
+    }
+
+    @Test
+    void findByNameIgnoreCase_ThrowAuthorNotFoundException_WhenAuthorIsNotFound(){
+        String authorNameTest = "Author Test Exception";
+        Assertions.assertThatExceptionOfType(AuthorNotFoundException.class)
+                .isThrownBy(() -> {
+                    repository
+                            .findByNameIgnoreCase(authorNameTest)
+                            .orElseThrow(() -> new AuthorNotFoundException(""));
+                });
+    }
 }
--- src/test/java/com/javadev/bookmanager/repository/BookRepositoryTest.java
+++ src/test/java/com/javadev/bookmanager/repository/BookRepositoryTest.java
@@ -1,10 +1,53 @@
 package com.javadev.bookmanager.repository;
 
+import com.javadev.bookmanager.entities.Book;
+import com.javadev.bookmanager.exceptions.AuthorNotFoundException;
+import com.javadev.bookmanager.exceptions.BookNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository repository;
+
+    private Book book;
+
+    @BeforeEach
+    void setup() {
+        book = new Book("Book Test", 9999, 999);
+        repository.save(book);
+    }
+
+    @AfterEach
+    void tearDown() {
+        book = null;
+        this.repository.deleteAll();
+    }
+
+    @Test
+    void findByNameIgnoreCase_returnBookByName_WhenSuccessful(){
+        Book bookTest = repository.findByNameIgnoreCase(book.getName()).orElse(new Book());
+        Assertions
+                .assertThat(bookTest)
+                .isNotNull()
+                .isEqualTo(book);
+    }
+
+    @Test
+    void findByNameIgnoreCase_ThrowBookNotFoundException_WhenBookIsNotFound(){
+        String bookNameTest = "Book Test Exception";
+        Assertions.assertThatExceptionOfType(BookNotFoundException.class)
+                .isThrownBy(() -> {
+                    repository
+                            .findByNameIgnoreCase(bookNameTest)
+                            .orElseThrow(() -> new BookNotFoundException(""));
+                });
+    }
 
 }

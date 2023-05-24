23aef8ed263930cb5300939f1672065c2f7bb956
Lukas Veiga <lukas.veiga@hotmail.com>
2023-05-23 21:34:45 -0300

    postgres testcontainers

---
+++ src/test/java/com/javadev/bookmanager/AbstractPostgresTestContainer.java
@@ -0,0 +1,21 @@
+package com.javadev.bookmanager;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractPostgresTestContainer {
+
+    @Container
+    private static final PostgreSQLContainer<?> container =
+            new PostgreSQLContainer<>("postgres:14-alpine");
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+}
